b79396e8254fd197f525b6e852d74ccf4ca12e5f
stephenong-wq <stephen.ong@savvywealth.com>
2026-05-18 13:21:47 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -385,7 +385,6 @@ export default function Home() {
                       </div>
                     </div>
                   )}
-                  )}
                 </div>
               </div>
 
